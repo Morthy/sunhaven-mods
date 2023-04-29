4df4ec5d75e0268003999cc02d12af8e53a401d0
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2023-04-29 21:56:33 +0200

    add what tf can i fish

---
+++ WhatFishCanICatch/FishUI.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Wish;
+
+namespace WhatFishCanICatch;
+
+public class FishUI : CustomUI
+{
+    private bool _isCreated = false;
+
+    public void Create()
+    {
+        if (_isCreated)
+        {
+            return;
+        }
+
+        var bg = this.CreateBg();
+        var title = this.CreateTitle(bg.transform);
+        CreateTitleText(title.transform, "Catchable fish");
+        var exit = this.CreateExitButton(this.transform);
+        exit.onClick.AddListener(() => {
+            UIHandler.Instance.CloseExternalUI();
+        });
+        
+        _scrollRect = this.CreateScrollView(
+            this.transform, 
+            new Vector3(0, 0, 0), 
+            new Vector2(210f, 30f),
+            new Vector2(0, 0),
+            new Vector2(210f, 330f),
+            new Vector2(210, 310),
+            new Vector2(10f, 200f),
+            new Vector2(100f, -8f)
+        );
+        
+        _isCreated = true;
+    }
+    
+    public void Populate(Dictionary<FishData, float> data)
+    {
+        foreach (var item in data)
+        {
+            AddFish(item.Key, item.Value);
+        }
+        
+        _scrollRect.verticalNormalizedPosition = 1f;
+    }
+
+    private void AddFish(FishData fish, float chance)
+    {
+        GameObject gameObject = new GameObject("Fish " + fish.id);
+        gameObject.layer = LayerMask.NameToLayer("UI");
+        var rootTransform = gameObject.AddComponent<RectTransform>();
+        rootTransform.SetParent(_scrollRect.content);
+        rootTransform.localScale = Vector3.one;
+        
+        Image image = new GameObject("ItemImage").AddComponent<Image>();
+        image.sprite = fish.icon;
+        image.color = Color.white;
+        image.preserveAspect = true;
+        image.gameObject.layer = LayerMask.NameToLayer("UI");
+        image.transform.SetParent(gameObject.transform);
+
+        RectTransform imageTransform = image.rectTransform;
+        imageTransform.localScale = Vector3.one;
+        imageTransform.localPosition = (Vector3) new Vector2(-83f, 0.0f);
+        imageTransform.sizeDelta = new Vector2(25, 25);
+
+        TextMeshProUGUI titleText = this.CreateText(rootTransform, "Fish name");
+        titleText.transform.localScale = Vector3.one;
+        titleText.transform.localPosition = new Vector3(-20, 0f, 0.0f);
+        titleText.transform.localRotation = Quaternion.identity;
+        titleText.rectTransform.sizeDelta = new Vector2(90, 25);
+        titleText.alignment = TextAlignmentOptions.Left;
+        titleText.fontSize = 18f;
+        titleText.fontSizeMax = 18f;
+        titleText.fontSizeMin = 16f;
+        titleText.enableWordWrapping = false;
+        titleText.text = fish.FormattedName;
+
+        TextMeshProUGUI percentageText = Instantiate(titleText, rootTransform);
+        percentageText.gameObject.name = "Percentage text";
+        percentageText.transform.localPosition = new Vector3(90f, 0f, 0.0f);
+        percentageText.text = Math.Round(chance*100, 2) + "%";
+        
+        Popup popup = image.gameObject.AddComponent<Popup>();
+        popup.name = "Popup";
+        popup.text = fish.FormattedName;
+        popup.description = fish.FormattedDescription;
+    }
+}
---
+++ WhatFishCanICatch/Plugin.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using Wish;
+
+namespace WhatFishCanICatch
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        public static ManualLogSource logger;
+        private static FishUI _fishUI;
+        private static ConfigEntry<string> _modifierKey;
+
+        private void Awake()
+        {            
+            logger = Logger;
+            
+            _modifierKey = Config.Bind<string>("General", "Mouse Button", "LeftControl", "Mouse button that should trigger opening the fishing info box");
+                     
+            this.harmony.PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} v{PluginInfo.PLUGIN_VERSION} is loaded!");
+        }
+
+        public static Dictionary<FishData, float> GetFishChancesFromFishData(RandomFishArray array, float fishingLevel)
+        {
+            float totalProbability = 0;
+            var dictionary = new Dictionary<FishData, float>();
+
+            float num1 = 0.0f;
+            float num2 = 0.0f;
+            if (GameSave.Fishing.GetNode("Fishing6b"))
+                num1 += 0.05f * GameSave.Fishing.GetNodeAmount("Fishing6b");
+            if (GameSave.Fishing.GetNode("Fishing10b"))
+                num2 += 0.1f * GameSave.Fishing.GetNodeAmount("Fishing10b");
+
+            for (int index = 0; index < array.drops.Length; ++index)
+            {
+                FishLoot drop = array.drops[index];
+                float num3 = 1f;
+                float num4;
+                switch (drop.fish.rarity)
+                {
+                    case ItemRarity.Epic:
+                        num4 = num3 + num1;
+                        break;
+                    case ItemRarity.Legendary:
+                        num4 = num3 + num1 + num2;
+                        break;
+                    default:
+                        num4 = num3 + num1;
+                        break;
+                }
+
+                var adjustedOdds = (float)typeof(RandomFishArray).GetMethod("AdjustedOddsBasedOnRarityAndLevel", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Invoke(array, new object[] { drop.fish.rarity, fishingLevel });
+                var chance = drop.dropChance * num4 * adjustedOdds;
+                totalProbability += chance;
+
+                dictionary[drop.fish] = chance;
+            }
+
+            var adjustedDictionary = new Dictionary<FishData, float>();
+
+            foreach (var item in dictionary)
+            {
+                adjustedDictionary[item.Key] = item.Value / totalProbability;
+            }
+
+            return adjustedDictionary;
+        }
+
+        [HarmonyPatch]
+        class Patches
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof (ItemIcon), "OnPointerDown")]
+            private static bool ItemIconOnPointerDown(ItemIcon __instance)
+            {
+                try
+                {
+                    if (!Input.GetKey((KeyCode)Enum.Parse(typeof(KeyCode),_modifierKey.Value)))
+                        return true;
+
+                    ItemData itemData = ItemDatabase.GetItemData(__instance.item);
+
+                    if (itemData.useItem && itemData.useItem.GetType() == typeof(FishingRod))
+                    {
+                        var closest = FindObjectsOfType<FishSpawner>().OrderBy(spawner => Vector2.Distance(spawner.transform.position, Player.Instance.transform.position)).FirstOrDefault();
+
+                        if (!closest)
+                        {
+                            SingletonBehaviour<NotificationStack>.Instance.SendNotification("No fishing locations found");
+                            return false;
+                        }
+
+                        UIHandler.Instance.CloseExternalUI(_fishUI.gameObject);
+                        
+                        closest.GetFish(); // Force currentFishSeason to be set
+                        var fishList = closest.currentFishSeason;
+
+                        var data = GetFishChancesFromFishData(fishList, Player.Instance.FishingSkill);
+                        data = (from entry in data orderby entry.Value descending select entry).ToDictionary(i => i.Key, i => i.Value);
+
+                        _fishUI.ClearContent();
+                        _fishUI.Populate(data);
+
+                        UIHandler.Instance.OpenUI(_fishUI.gameObject, _fishUI.gameObject.transform.parent);
+                        return false;
+                    }
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    logger.LogInfo($"{PluginInfo.PLUGIN_GUID}: {e}");
+                    return true;
+                }
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(UIHandler), "Initialize")]
+            private static void UIHandlerInitialize(UIHandler __instance)
+            {
+                if (_fishUI)
+                {
+                    return;
+                }
+                
+                _fishUI = new GameObject("What can I catch").AddComponent<FishUI>();
+                _fishUI.gameObject.SetActive(false);
+                _fishUI.transform.SetParent(UIHandler.Instance.transform);
+                _fishUI.transform.localPosition = Vector3.zero;
+                _fishUI.transform.localRotation = Quaternion.identity;
+                _fishUI.transform.localScale = new Vector3(1, 1, 1);
+                _fishUI.transform.SetAsLastSibling();
+                _fishUI.Create();
+            }
+        }
+        
+    }
+
+}
+
