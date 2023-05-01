12523615543523b1d6ebca070795ce9624c6f836
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2023-05-01 02:42:07 +0200

    Add decorate anywhere

---
+++ DecorateAnywhere/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+
+namespace DecorateAnywhere
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {            
+            logger = Logger;
+            harmony.PatchAll();
+            
+            harmony.Patch(typeof(GameManager).GetMethods().First(
+                    m => m.Name.Equals("InvalidDecorationPlacement") &&  m.GetParameters().Length == 8), 
+                null, 
+                new HarmonyMethod(typeof(Patches).GetMethod("GameManagerInvalidDecorationPlacement"))
+            );
+            
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} v{PluginInfo.PLUGIN_VERSION} is loaded!");
+        }
+
+        [HarmonyPatch]
+        class Patches
+        {
+            public static void GameManagerInvalidDecorationPlacement(ref bool canBePlaced)
+            {
+                canBePlaced = true; // :)
+            }
+        }
+        
+    }
+}
