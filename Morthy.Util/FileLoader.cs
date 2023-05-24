c6866a57c54f9eb5c42817771a4e6ed66cbce864
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2023-05-24 04:33:59 +0200

    Add util

---
+++ Morthy.Util/FileLoader.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Morthy.Util;
+
+public static class FileLoader {
+    private static string GetResourcePath(Assembly assembly, string name)
+    {
+        string resourcePath = name;
+        if (!name.StartsWith(assembly.GetName().Name))
+        {
+            resourcePath = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(name));
+        }
+
+        return resourcePath;
+    }
+    
+    public static string LoadFile(Assembly assembly, string name)
+    {
+        using (var stream = assembly.GetManifestResourceStream(GetResourcePath(assembly, name)))
+        using (var reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+    
+    public static byte[] LoadFileBytes(Assembly assembly, string name)
+    {
+        using (var stream = assembly.GetManifestResourceStream(GetResourcePath(assembly, name)))
+        using (var reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                reader.BaseStream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ Morthy.Util/SpriteUtil.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Morthy.Util;
+
+public class SpriteUtil
+{
+    private static Texture2D CreateTexture(byte[] data)
+    {
+        var texture = new Texture2D(1, 1);
+        texture.LoadImage(data);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.wrapModeU = TextureWrapMode.Clamp;
+        texture.wrapModeV = TextureWrapMode.Clamp;
+        texture.wrapModeW = TextureWrapMode.Clamp;
+        return texture;
+    }
+
+    public static Sprite CreateSprite(byte[] textureData, string name)
+    {
+        var texture = CreateTexture(textureData);
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 24);
+        sprite.name = name;
+        texture.name = name;
+        return sprite;
+    }
+    
+    public static Sprite CreateSpriteDecoration(byte[] textureData, string name)
+    {
+        var texture = CreateTexture(textureData);
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0f, 0f), 24,  0, SpriteMeshType.FullRect);
+        sprite.name = name;
+        texture.name = name;
+        return sprite;
+    }
+    
+    public static Sprite CreateSprite(byte[] textureData, Vector2 pivot, string name)
+    {
+        var texture = CreateTexture(textureData);
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), pivot, 24,  0, SpriteMeshType.FullRect);
+        sprite.name = name;
+        texture.name = name;
+        return sprite;
+    }
+}
