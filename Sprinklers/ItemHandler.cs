b89f05ce5e7fd495c55f21b352c36a43c7b7303c
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2023-05-24 04:34:27 +0200

    Add sprinklers

---
+++ Sprinklers/ItemHandler.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Morthy.Util;
+using UnityEngine;
+using Wish;
+using Object = UnityEngine.Object;
+
+namespace Sprinklers;
+
+public static class ItemHandler
+{
+    private const int BaseSprinklerId = 10535;
+    
+    private const int SmallSprinklerId = 30011;
+    private const int LargeSprinklerId = 30012;
+    private const int NelvariSprinklerId = 30013;
+    private const int WithergateSprinklerId = 30014;
+
+    private static bool _itemsCreated = false;
+    private static void CreateSprinklerItem(int id, int range)
+    {
+        if (ItemDatabase.GetItemData(id) && ItemDatabase.GetItemData(id).name != "Decoration Catalogue")
+        {
+            Plugin.logger.LogError($"Cannot create modded item with ID {id} because it is already in use by a different item.");
+            return;
+        }
+
+        var original = ItemDatabase.GetItemData(BaseSprinklerId);
+
+        if (!original)
+        {
+            Plugin.logger.LogError("Cannot create modded Sprinkler because the original sprinkler no longer exists.");
+            return;
+        }
+
+        var item = ScriptableObject.CreateInstance<ItemData>();
+        JsonUtility.FromJsonOverwrite(FileLoader.LoadFile(Assembly.GetExecutingAssembly(), $"data.{id}.json"), item);
+        item.icon = SpriteUtil.CreateSprite(FileLoader.LoadFileBytes(Assembly.GetExecutingAssembly(), $"img.{id}.png"), $"Modded item icon {id}");
+
+        var useItem = Object.Instantiate(original.useItem) as Placeable;
+
+        if (!useItem)
+        {
+            Plugin.logger.LogError("Original sprinkler has no useItem");
+            return;
+        }
+        
+        Object.Destroy(useItem.GetComponent<PlaceableSpriteLoader>());
+        useItem._previewSprite = SpriteUtil.CreateSpriteDecoration(FileLoader.LoadFileBytes(Assembly.GetExecutingAssembly(), $"img.{id}.png"), $"Modded item decoration {id}");
+        useItem.previewOffset = new Vector2(-0.08f, 0);
+        useItem._itemData = item;
+
+        var p = 0.5f - (1f / (range * 2 + 1) / 2);
+        useItem._secondaryPreviewSprite = SpriteUtil.CreateSprite(FileLoader.LoadFileBytes(Assembly.GetExecutingAssembly(), $"preview_{range}.png"), new Vector2(p, p), $"Modded item secondary preview {id}");
+        
+        item.useItem = useItem;
+
+        var sprinkler = Object.Instantiate(useItem._decoration) as Sprinkler;
+
+        if (!sprinkler)
+        {
+            Plugin.logger.LogError("Original sprinkler has no decoration of type Sprinkler");
+            return;
+        }
+        
+        useItem._decoration = sprinkler;
+        sprinkler.range = range;
+        
+        var graphics = sprinkler.transform.Find("Graphics");
+
+        Object.Destroy(graphics.gameObject.GetComponent<MeshGenSpriteLoader>());
+        var mg = graphics.gameObject.GetComponent<MeshGenerator>();
+        mg.sprite = useItem._previewSprite;
+        mg.SetDefault();
+
+        graphics.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        useItem.gameObject.SetActive(false);
+        
+        Object.DontDestroyOnLoad(useItem);
+        Object.DontDestroyOnLoad(sprinkler);
+
+        ItemDatabase.items[item.id] = item;
+        ItemDatabase.ids[item.name.RemoveWhitespace().ToLower()] = item.id;
+        Plugin.logger.LogDebug($"Created item {item.id} with name {item.name}");
+    }
+
+    private static void AddItemToRecipeList(int id, string recipeList, List<ItemInfo> input)
+    {
+        
+        foreach (var rl in Resources.FindObjectsOfTypeAll<RecipeList>())
+        {
+            if (!rl.name.Equals(recipeList)) continue;
+            
+            if (rl.craftingRecipes.Any(r => r.output.item.id == id))
+            {
+                return;
+            }
+                
+            var recipe = ScriptableObject.CreateInstance<Recipe>();
+            recipe.output = new ItemInfo { item = ItemDatabase.GetItemData(id), amount = 1 };
+            recipe.input = input;
+            recipe.worldProgressTokens = new List<Progress>();
+            recipe.characterProgressTokens = new List<Progress>();
+            recipe.questProgressTokens = new List<QuestAsset>();
+            recipe.hoursToCraft = 1f;
+                
+            rl.craftingRecipes.Add(recipe);
+            Plugin.logger.LogDebug($"Added item {id} to {recipeList}");
+        }
+    }
+
+    public static void CreateSprinklerItems()
+    {
+        if (_itemsCreated)
+        {
+            return;
+        }
+        
+        CreateSprinklerItem(SmallSprinklerId, 1);
+        CreateSprinklerItem(LargeSprinklerId, 2);
+        CreateSprinklerItem(NelvariSprinklerId, 3);
+        CreateSprinklerItem(WithergateSprinklerId, 4);
+        
+        AddItemToRecipeList(30011, "RecipeList_Anvil", new List<ItemInfo>
+        {
+            new() { item = ItemDatabase.GetItemData(ItemID.GoldBar), amount = 5 },
+            new() { item = ItemDatabase.GetItemData(ItemID.WaterCrystal), amount = 1 }
+        });
+        
+        AddItemToRecipeList(30012, "RecipeList_Anvil", new List<ItemInfo>
+        {
+            new() { item = ItemDatabase.GetItemData(ItemID.SuniteBar), amount = 5 },
+            new() { item = ItemDatabase.GetItemData(ItemID.Havenite), amount = 1 }
+        });
+        
+        AddItemToRecipeList(30013, "RecipeList_Mana Anvil", new List<ItemInfo>
+        {
+            new() { item = ItemDatabase.GetItemData(ItemID.EnchantedMithrilBar), amount = 5 },
+            new() { item = ItemDatabase.GetItemData(ItemID.WaterRune), amount = 1 }
+        });
+        
+        AddItemToRecipeList(30014, "RecipeList_Monster Anvil", new List<ItemInfo>
+        {
+            new() { item = ItemDatabase.GetItemData(ItemID.EnhancedMithrilBar), amount = 5 },
+            new() { item = ItemDatabase.GetItemData(ItemID.RefinedGlass), amount = 1 }
+        });
+        
+        _itemsCreated = true;
+    }
+}
---
+++ Sprinklers/Plugin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+
+namespace Sprinklers;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+    public static ManualLogSource logger;
+    
+    private void Awake()
+    {
+        logger = Logger;
+        _harmony.PatchAll();
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} v{PluginInfo.PLUGIN_VERSION} is loaded!");
+    }
+    
+    
+    [HarmonyPatch]
+    private class Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MainMenuController), "Awake")]
+        public static void MainMenuControllerAwake()
+        {
+            try
+            {
+                ItemHandler.CreateSprinklerItems();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Placeable), "OnDisable")]
+        public static bool PlaceableOnDisable(ref Placeable __instance)
+        {
+            try
+            {
+                if (!__instance.Player)
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
+            
+            return true;
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UseItem), "SetPlayer")]
+        public static bool UseItemSetPlayer(ref UseItem __instance)
+        {
+            try
+            {
+                if (!__instance.gameObject.activeSelf)
+                {
+                    __instance.gameObject.SetActive(true);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
+
+            return true;
+        }
+    }
+}
