c215af67f37093f0c37eef9219548689861658de
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2024-06-13 00:04:45 +0200

    Latest changes

--- Sprinklers/CustomSprinkler.cs
+++ Sprinklers/CustomSprinkler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.Events;
 using Wish;
 using ZeroFormatter;
@@ -9,16 +10,16 @@ public class CustomSprinkler : Decoration
 {
     public int range;
 
-    public override int UpdateOrder => 1000;
+    public override int UpdateOrder => 101;
 
     public override void LateUpdateMetaOvernight(ref DecorationPositionData decorationData)
     {
       base.LateUpdateMetaOvernight(ref decorationData);
-      for (int index1 = -this.range; index1 <= this.range; ++index1)
+      for (int index1 = -range; index1 <= range; ++index1)
       {
-        for (int index2 = -this.range; index2 <= this.range; ++index2)
+        for (int index2 = -range; index2 <= range; ++index2)
         {
-          Vector2Int vector2Int = new Vector2Int((int) decorationData.x + index1 * 6, (int) decorationData.y + index2 * 6);
+          Vector2Int vector2Int = new Vector2Int(decorationData.x + index1 * 6, decorationData.y + index2 * 6);
           DecorationPositionData decorationPositionData;
           if (SingletonBehaviour<GameSave>.Instance.CurrentWorld.Decorations[decorationData.sceneID].TryGetValue(new KeyTuple<ushort, ushort, sbyte>((ushort) vector2Int.x, (ushort) vector2Int.y, decorationData.z), out decorationPositionData))
           {
@@ -26,25 +27,27 @@ public class CustomSprinkler : Decoration
             {
               if (ItemDatabase.GetItemData(decorationPositionData.id).useItem is Seeds)
               {
-                CropSaveData data = (CropSaveData) null;
-                if (Decoration.DeserializeMeta<CropSaveData>(decorationPositionData.meta, ref data) && !data.watered)
+                CropSaveData data = null;
+                if (DeserializeMeta(decorationPositionData.meta, ref data) && (!data.watered || data.onFire))
                 {
+
+                  data.onFire = false;
                   data.watered = true;
-                  byte[] meta = ZeroFormatterSerializer.Serialize<CropSaveData>(data);
-                  Vector3Int position = new Vector3Int((int) decorationPositionData.x, (int) decorationPositionData.y, (int) decorationPositionData.z);
+                  byte[] meta = ZeroFormatterSerializer.Serialize(data);
+                  Vector3Int position = new Vector3Int(decorationPositionData.x, decorationPositionData.y, decorationPositionData.z);
                   short sceneId = decorationPositionData.sceneID;
                   decorationPositionData.meta = meta;
                   UnityAction<Vector3Int, short, byte[]> decorationMetaUpdated = GameManager.onDecorationMetaUpdated;
                   if (decorationMetaUpdated != null)
                     decorationMetaUpdated(position, sceneId, meta);
-                  SingletonBehaviour<GameManager>.Instance.UpdateDecorationMetaFromRPC(position, (int) sceneId, meta);
+                  SingletonBehaviour<GameManager>.Instance.UpdateDecorationMetaFromRPC(position, sceneId, meta);
                 }
               }
             }
             else
               continue;
           }
-          Vector2Int position1 = new Vector2Int((int) decorationData.x / 6 + index1, (int) decorationData.y / 6 + index2);
+          Vector2Int position1 = new Vector2Int(decorationData.x / 6 + index1, decorationData.y / 6 + index2);
           if (SingletonBehaviour<TileManager>.Instance.IsWaterable(position1, decorationData.sceneID))
             SingletonBehaviour<TileManager>.Instance.Water(position1, decorationData.sceneID);
         }
