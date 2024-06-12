e94dd34f9bef870af81f16379b37ba08834687e1
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2024-06-13 00:54:47 +0200

    Fix QuickMount for 1.4

--- QuickMount/Plugin.cs
+++ QuickMount/Plugin.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using PSS;
 using UnityEngine;
 using Wish;
 
@@ -36,59 +38,6 @@ namespace QuickMount
         [HarmonyPatch]
         class Patches
         {
-            /*
-            // ReInput.mapping.Actions
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(ReInput.MappingHelper), "Actions", MethodType.Getter)]
-            public static void ReInputMappingHelper(ref IList<InputAction> __result)
-            {
-                if (__result == null)
-                {
-                    return;
-                }
-
-                logger.LogInfo(__result);
-
-                var action = new InputAction();
-                typeof(InputAction).GetProperty("id").SetValue(action, 100);
-                logger.LogInfo("yyy");
-                typeof(InputAction).GetProperty("name").SetValue(action, "Mount/unmount");
-                typeof(InputAction).GetProperty("type").SetValue(action, InputActionType.Button);
-                typeof(InputAction).GetProperty("descriptiveName").SetValue(action, "Mount/unmount");
-                logger.LogInfo("xxx");
-                typeof(InputAction).GetProperty("positiveDescriptiveName").SetValue(action, "Mount/unmount");
-                typeof(InputAction).GetProperty("negativeDescriptiveName").SetValue(action, "Mount/unmount");
-                typeof(InputAction).GetProperty("behaviorId").SetValue(action, 0);
-                typeof(InputAction).GetProperty("userAssignable").SetValue(action, true);
-                typeof(InputAction).GetProperty("categoryId").SetValue(action, 1);
-
-                var newCollection = new List<InputAction>(__result) {  };
-                __result = newCollection;
-            }
-            
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(Keybinds), "SetupKeybindButtons")]
-            public static void KeybindsSetupKeybindButtons(ref Keybinds __instance)
-            {
-                typeof(Keybinds).GetMethod("SetupKeybindFromSettings", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, new object[] { (Button)100, false });
-            }
-
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(Keybinds), "GetButtonName")]
-            public static bool KeybindsGetButtonName(ref Button button, ref string __result)
-            {
-                if ((int)button == 100)
-                {
-                    __result = "Mount/unmount";
-                    return false;
-                }
-
-                return true;
-            }
-            */
-            
-            // GetButtonName(Button button)
-            
             [HarmonyPrefix]
             [HarmonyPatch(typeof(Wish.Player), "Update")]
             public static void PlayerUpdatePostfix(ref Wish.Player __instance)
@@ -97,35 +46,38 @@ namespace QuickMount
                 {
                     return;
                 }
+
+                if (!Input.GetKeyDown((KeyCode)Enum.Parse(typeof(KeyCode), HotKey.Value))) return;
                 
-                if (Input.GetKeyDown((KeyCode)Enum.Parse(typeof(KeyCode), HotKey.Value)))
+                var found = false;
+
+                foreach (var slotItemData in __instance.Inventory.Items.Where(slotItemData => slotItemData.item != null && slotItemData.amount > 0))
                 {
-                    UseItem useItem = null;
-
-                    foreach (var slotItemData in __instance.Inventory.Items)
+                    logger.LogInfo("get info");
+                    Database.GetData<ItemData>(slotItemData.item.ID(), (itemData) =>
                     {
-                        if (slotItemData.item != null && slotItemData.amount > 0)
+                        logger.LogInfo("got info");
+                        if (!found && itemData.useItem && itemData.useItem.GetType() == typeof(MountWhistle))
                         {
-                            var itemData = ItemDatabase.GetItemData(slotItemData.item.ID());
-
-
-                            if (itemData.useItem && itemData.useItem.GetType() == typeof(MountWhistle))
-                            {
-                                useItem = itemData.useItem;
-                                break;
-                            }
+                            found = true;
+                                    
+                            itemData.useItem.SetPlayer(Player.Instance);
+                            itemData.useItem.UseDown1();
+                                    
                         }
-                    }
+                    });
 
-                    if (useItem)
+                    if (found)
                     {
-                        useItem.SetPlayer(__instance);
-                        useItem.UseDown1();
+                        break;
                     }
-                    else
-                    {
-                        NotificationStack.Instance.SendNotification("No mount whistle found in your inventory.");
-                    }
+                }
+                    
+                logger.LogInfo("finished");
+
+                if (!found)
+                {
+                    NotificationStack.Instance.SendNotification("No mount whistle found in your inventory.");
                 }
             }
         }
