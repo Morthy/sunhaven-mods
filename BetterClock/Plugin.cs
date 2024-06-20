d37c0faee882d6569d2c8c01ab4d6d57eee0405b
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2024-06-21 00:48:19 +0200

    add better clock

---
+++ BetterClock/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using TMPro;
+using UnityEngine.UI;
+using Wish;
+
+namespace BetterClock;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+
+    private void Awake()
+    {
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch]
+    class Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(DayCycle), "UpdateTimeText")]
+        public static void DayCycleUpdateTimeText(ref TextMeshProUGUI ____timeTMP)
+        {
+            var text = DayCycle.Instance.Time.ToString("HH:") + (DayCycle.Instance.Time.Minute / 10 * 10).ToString("00");
+
+            if (DayCycle.Instance.Time.Hour >= 22 || DayCycle.Instance.Time.Hour <= 0)
+            {
+                text = $"<color=red>{text}</color>";
+            }
+
+            ____timeTMP.text = text;
+        }
+    }
+}
