16f79a7994094fdc21d629cbf6a18b06138fc000
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2024-06-29 03:24:18 +0200

    Add teleport items

---
+++ TeleportItems/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace TeleportItems;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("CustomItems", "0.3.0")]
+public class Plugin : BaseUnityPlugin
+{
+
+}
---
+++ TeleportItems/TeleportItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Wish;
+
+namespace TeleportItems;
+
+public class TeleportItem : UseItem
+{
+    public string teleportScene;
+    public Vector2 teleportLocation;
+
+    public override void Use1()
+    {
+        DialogueController.Instance.SetDefaultBox();
+        DialogueController.Instance.PushDialogue(new DialogueNode()
+        {
+            dialogueText = new List<string>() { $"Are you sure you wish to use your teleporter? This will consume the item." },
+            responses = new Dictionary<int, Response>()
+            {
+                {
+                    1,
+                    new Response
+                    {
+                        responseText = () => "Yes",
+                        action = () =>
+                        {
+                            ScenePortalManager.Instance.ChangeScene(teleportLocation, teleportScene);
+                            Player.Instance.Inventory.RemoveItemAt(Player.Instance.ItemIndex, 1);
+                        }
+                    }
+                },
+                {
+                    2,
+                    new Response
+                    {
+                        responseText = () => "No",
+                        action = null
+                    }
+                },
+            }
+        });
+    }
+}
