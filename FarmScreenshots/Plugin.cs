d69db8c1530ac57c0ab5603b1c7a489ac8c0c0cb
Jon Dawson <5537959+Morthy@users.noreply.github.com>
2024-07-15 23:56:46 +0200

    farm screenshots mod

---
+++ FarmScreenshots/Plugin.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using DG.Tweening;
+using HarmonyLib;
+using PSS;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Wish;
+
+namespace FarmScreenshots;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+    public static ManualLogSource logger;
+    public static ScreenshotMaker ScreenshotMaker;
+    private static ConfigEntry<string> HotKey;
+
+
+    private void Awake()
+    {
+        logger = Logger;
+        HotKey = this.Config.Bind<string>("General", "Screenshot Hotkey", "F11", "Unity Keycode to open the menu");
+        this.harmony.PatchAll();
+        
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} v{PluginInfo.PLUGIN_VERSION} is loaded!");
+    }
+
+    [HarmonyPatch]
+    private class Patches
+    { 
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Wish.Player), "Update")]
+        public static void PlayerUpdatePostfix(ref Wish.Player __instance)
+        {
+            if (!__instance.IsOwner || Cutscene.Active || __instance.Pathing || UIHandler.InventoryOpen || __instance.pause || __instance.Sleeping)
+            {
+                return;
+            }
+
+
+            if (!(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown((KeyCode) Enum.Parse(typeof(KeyCode), HotKey.Value))))
+            {
+                return;
+            }
+
+            if (ScreenshotMaker != null)
+            {
+                return;
+            }
+
+            logger.LogInfo(SceneManager.GetActiveScene().name);
+
+            if (SceneManager.GetActiveScene().name.Equals("2playerfarm"))
+            {
+                var smObject = new GameObject("ScreenshotMaker");
+                ScreenshotMaker = smObject.AddComponent<ScreenshotMaker>();
+                ScreenshotMaker.cameraPosition = new Vector3(331.9659f, 60.1794f, -50f);
+                ScreenshotMaker.orthographicSize = 45f;
+                ScreenshotMaker.aspectRatio = 1f;
+                ScreenshotMaker.screenshotWidth = 5000;
+                ScreenshotMaker.screenshotHeight = 5000;
+            }
+            else if (SceneManager.GetActiveScene().name.Equals("NelvariFarm"))
+            {
+                var smObject = new GameObject("ScreenshotMaker");
+                ScreenshotMaker = smObject.AddComponent<ScreenshotMaker>();
+                ScreenshotMaker.cameraPosition = new Vector3(107.5376f, 33.8658f,-50f);
+                ScreenshotMaker.orthographicSize = 22f;
+                ScreenshotMaker.aspectRatio = 1.8f;
+                ScreenshotMaker.screenshotWidth = 3000;
+                ScreenshotMaker.screenshotHeight = 1666;
+            }
+            else if (SceneManager.GetActiveScene().name.Equals("WithergateRooftopFarm"))
+            {
+                var smObject = new GameObject("ScreenshotMaker");
+                ScreenshotMaker = smObject.AddComponent<ScreenshotMaker>();
+                ScreenshotMaker.cameraPosition = new Vector3(140.7435f, 17.7507f,-50f);
+                ScreenshotMaker.orthographicSize = 24f;
+                ScreenshotMaker.aspectRatio = 1.8f;
+                ScreenshotMaker.screenshotWidth = 3000;
+                ScreenshotMaker.screenshotHeight = 1666;
+            }            
+            else
+            {
+                NotificationStack.Instance.SendNotification("Screenshots are not supported in this location.");
+            }
+
+            if (!ScreenshotMaker) return;
+            
+            
+            NotificationStack.Instance.SendNotification("Taking screenshot...");
+            DOVirtual.DelayedCall(1f, () => ScreenshotMaker.TakeScreenshot());
+        }
+    }
+}
---
+++ FarmScreenshots/ScreenshotMaker.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEngine;
+using Wish;
+
+namespace FarmScreenshots;
+
+public class ScreenshotMaker : MonoBehaviour
+{
+    public Vector3 cameraPosition;
+    public float orthographicSize;
+    public float aspectRatio;
+    public int screenshotWidth;
+    public int screenshotHeight;
+    private GameObject _cameraObject;
+    private readonly List<GameObject> _temporaryHidden = new();
+    private int _waitCount = 0;
+    
+    public void Awake()
+    {
+        Plugin.logger.LogInfo("Awake");
+    }
+
+    public void TakeScreenshot()
+    {
+        //HideStuff();
+        _cameraObject = Instantiate(Player.Instance.Camera.gameObject, this.transform);
+        _cameraObject.transform.eulerAngles = new Vector3(315f, 0, 0);
+        _cameraObject.transform.position = cameraPosition;
+        var renderCamera = _cameraObject.GetComponent<Camera>();
+        renderCamera.enabled = false;
+        renderCamera.forceIntoRenderTexture = true;
+        renderCamera.orthographic = true;
+        renderCamera.orthographicSize = orthographicSize;
+        renderCamera.aspect = aspectRatio;
+        renderCamera.targetDisplay = 0;
+        renderCamera.backgroundColor = new Color(0.0198f, 0.0249f, 0.0377f, 1f);
+        
+        Camera.onPostRender += OnCameraPostRender;
+
+        renderCamera.enabled = true;
+        
+        var renderTexture = new RenderTexture(screenshotWidth, screenshotHeight, 24);
+        renderCamera.targetTexture = renderTexture;
+        renderCamera.Render();
+    }
+
+    private void OnCameraPostRender(Camera renderCamera)
+    {
+        if (renderCamera != _cameraObject.GetComponent<Camera>())
+        {
+            return;
+        }
+
+        _waitCount++;
+
+        if (_waitCount < 10)
+        {
+            return;
+        }
+
+        try
+        {
+            var image = new Texture2D(renderCamera.targetTexture.width, renderCamera.targetTexture.height, TextureFormat.RGB24, false);
+            image.ReadPixels(new Rect(0, 0, renderCamera.targetTexture.width, renderCamera.targetTexture.height), 0, 0);
+            image.Apply();
+
+            var bytes = image.EncodeToPNG();
+
+            var screenshotDir = Path.Combine(BepInEx.Paths.PluginPath, "Screenshots");
+
+            if (!Directory.Exists(screenshotDir))
+            {
+                Directory.CreateDirectory(screenshotDir);
+            }
+
+            var screenshotFile = Path.Combine(screenshotDir, SingletonBehaviour<GameSave>.Instance.CurrentSave.characterData.characterName + " " + DateTime.Now.ToString("yyyyMMddhhmmss") + ".png");
+
+            Plugin.logger.LogInfo(screenshotFile);
+
+            if (!File.Exists(screenshotFile))
+            {
+                File.WriteAllBytes(screenshotFile, bytes);
+                NotificationStack.Instance.SendNotification("Screenshot saved successfully!");
+            }
+
+            RenderTexture.active = null;
+            renderCamera.targetTexture = null;
+            renderCamera.enabled = false;
+        }
+        catch (Exception e)
+        {
+            Plugin.logger.LogError(e);
+        }
+
+        Camera.onPostRender -= OnCameraPostRender;
+        
+        //ShowStuff();
+        Destroy(gameObject);
+    }
+
+    private void HideStuff()
+    {
+
+    }
+
+    private void ShowStuff()
+    {
+        
+    }
+}
